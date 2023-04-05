ef5cc50fd8dec1a4d17eb6026a471802f86751c4
Abhi Yerra <abhi@berkeley.edu>
2023-04-05 13:30:49 -0700

    Added

--- efs.tf
+++ efs.tf
@@ -7,8 +7,6 @@ resource "aws_efs_mount_target" "this" {
   file_system_id  = aws_efs_file_system.this.id
   subnet_id       = var.subnet_id
   security_groups = var.node_security_group_ids
-
-  tags = var.tags
 }
 
 resource "kubernetes_storage_class_v1" "this" {
@@ -20,8 +18,6 @@ resource "kubernetes_storage_class_v1" "this" {
     provisioningMode = "efs-ap"
     fileSystemId     = aws_efs_file_system.this.id
     directoryPerms   = "700"
-    gidRangeStart    = "1000" # optional
-    gidRangeEnd      = "2000" # optional
     basePath         = var.path
   }
 }
