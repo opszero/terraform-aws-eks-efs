a06d0f69a8fa690ae2b7d66186b0fcd149f0b6e2
Jana <jana@opszero.com>
2023-10-09 21:11:51 +0200

    EFS > Update creation and storage class
    Using this method https://github.com/kubernetes-sigs/aws-efs-csi-driver/blob/master/examples/kubernetes/dynamic_provisioning/README.md

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-  description = "EFS file system bame"
+  description = "EFS file system name"
 }
 
 variable "subnet_ids" {
@@ -10,6 +10,21 @@ variable "subnet_ids" {
 variable "security_group_ids" {
   default     = []
   description = "Kubernetes node security group id"
+}
+
+variable "vpc_id" {
+  default = ""
+  description = "EKS cluster vpc id"
+}
+
+variable "vpc_cidr_block" {
+  default = ""
+  description = "VPC CIDR block for allowing access in efs system"
+}
+
+variable "efs_storage_size" {
+  default = "5Gi"
+  description = "Storage requested by kubernetes"
 }
 
 variable "tags" {
